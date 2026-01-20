a28323a237d63dcca83d1b3d8a79f9ddde080e37
Citraaaft <citrafatika1802@gmail.com>
2026-01-20 08:01:15 +0700

    Ubah logo CDC

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,9 @@
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
 
-            <title inertia>{{ config('app.name', 'Laravel') }}</title>
+           <title inertia></title>
+
+            <link rel="icon" type="image/png" href="{{ asset('images/logo-cdc-bulet.png') }}">
 
             <!-- Fonts -->
             <link rel="preconnect" href="https://fonts.bunny.net">
